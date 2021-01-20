d035a202ce6b960db71f49289574adb942ac1fbe
Nathan Solomon <nbsolomon4559@gmail.com>
2021-01-20 06:47:17 -0600

    initial github workflow setup and pyinstaller integration

--- dartboard/dartboard_main.py
+++ dartboard/dartboard_main.py
@@ -8,6 +8,11 @@
 from backend import django_setup
 import painter_example
 
+# Note: this is used to make our workflows actually grab all the classes
+# Some classes may not be directly called by our code and adding those
+# to the extra_imports file allows those to be picked up by the installer
+import extra_imports
+
 # Opens a new Painter Example window.
 # This should be the last thing executed here or should
 # be moved to execute on it's own thread
---
+++ dartboard/extra_imports.py
@@ -0,0 +1,11 @@
+#####################
+# extra_imports.py
+#
+# This file is used to import any thing that needs to be imported
+# but somehow didn't get picked up. Most of these are django things
+# called by the django backend that aren't naturally picked up
+# by our installation scripts
+#####################
+import backend.apps
+import backend.models
+import backend.migrations
