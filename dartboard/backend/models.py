b1bee6e74d096e87285b8d7eead652b5eb5d4ab4
Nathan Solomon <nbsolomon4559@gmail.com>
2021-01-25 14:01:57 -0800

    Initial Project Setup and Main Component Demos (#1)
    * Adding .gitignore and updating to ignore pycharm IDE files
    
    * adding support for a django backend
    
    * adding documentation for overall installation and django
    
    * removing pycache directories
    
    * updating .gitignore to include standard python contents
    
    * initial github workflow setup and pyinstaller integration
    
    * fixing python version
    
    * trying a different python version
    
    * trying a different docker container
    
    * trying adding a pythong container
    
    * trying python container
    
    * adding basic pytest
    
    * trying specifying current directory for pytest
    
    * trying manually specifying test path
    
    * listing directory to find why tests dont exist
    
    * adding back accidental removal of checkout stage
    
    * changing windows stage to use python container
    
    * adding wine to workflow configuration
    
    * updating packages
    
    * trying 32-bit wine
    
    * giving wine a little help
    
    * removing previously existing directory (I hope we didn't need that
    
    * reordering commands for possible caching
    
    * removing directory shenanigens that were causing caching issues
    
    * trying winecfg
    
    * updating ownership of directory
    
    * fixing possible syntax error on chown command
    
    * adding some lines for debugging
    
    * updating to new group
    
    * a different container version
    
    * configuring python to install inside wine
    
    * fixing typos
    
    * adding documentation and doxygen builds
    
    * fixing workflow syntax
    
    * adding code owners
    
    * resolving doxygen comment formatting issues
    
    * Removing cached files from tracking

---
+++ dartboard/backend/models.py
@@ -0,0 +1,17 @@
+###################
+# Contains all of the models
+# to be included in the database
+###################
+import uuid
+from django.db import models
+
+
+##
+# This is a basic model class designed to show backend proof of concept
+class DartboardHit(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    x = models.IntegerField(default=0)
+    y = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.x) + ', ' + str(self.y)
