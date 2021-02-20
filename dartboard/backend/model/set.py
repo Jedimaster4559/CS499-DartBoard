583ee2fa58f9a7bd979f468fe51983f0bfe35845
Nathan Solomon <nbsolomon4559@gmail.com>
2021-02-20 13:56:53 -0600

    adding more player stats and full names

--- dartboard/backend/model/set.py
+++ dartboard/backend/model/set.py
@@ -10,3 +10,4 @@ class Set(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     match = models.ForeignKey(Match, on_delete=models.CASCADE)
     best_of_legs_number = models.IntegerField(default=5)
+    num_legs_complete = models.IntegerField(default=0)
