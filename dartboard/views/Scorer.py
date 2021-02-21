4d7134886e640ddb3c47d5a08413b4968ecdb39f
Ethan Shattuck <ethanshattuck44@gmail.com>
2021-02-21 15:13:35 -0600

    Added table entries

--- dartboard/views/Scorer.py
+++ dartboard/views/Scorer.py
@@ -19,7 +19,7 @@ class Ui_Scorer(object):
     def setupUi(self, Scorer):
         if not Scorer.objectName():
             Scorer.setObjectName(u"Scorer")
-        Scorer.resize(719, 527)
+        Scorer.resize(723, 527)
         self.centralwidget = QWidget(Scorer)
         self.centralwidget.setObjectName(u"centralwidget")
         self.gridLayout = QGridLayout(self.centralwidget)
@@ -48,16 +48,6 @@ class Ui_Scorer(object):
 
         self.gridLayout.addWidget(self.NextSetButton, 3, 5, 1, 1)
 
-        self.EndMatchButton = QPushButton(self.centralwidget)
-        self.EndMatchButton.setObjectName(u"EndMatchButton")
-        sizePolicy1 = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
-        sizePolicy1.setHorizontalStretch(0)
-        sizePolicy1.setVerticalStretch(0)
-        sizePolicy1.setHeightForWidth(self.EndMatchButton.sizePolicy().hasHeightForWidth())
-        self.EndMatchButton.setSizePolicy(sizePolicy1)
-
-        self.gridLayout.addWidget(self.EndMatchButton, 5, 3, 1, 1)
-
         self.SetNumberLabel = QLabel(self.centralwidget)
         self.SetNumberLabel.setObjectName(u"SetNumberLabel")
 
@@ -70,26 +60,31 @@ class Ui_Scorer(object):
 
         self.tabWidget = QTabWidget(self.centralwidget)
         self.tabWidget.setObjectName(u"tabWidget")
-        sizePolicy2 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        sizePolicy2.setHorizontalStretch(0)
-        sizePolicy2.setVerticalStretch(0)
-        sizePolicy2.setHeightForWidth(self.tabWidget.sizePolicy().hasHeightForWidth())
-        self.tabWidget.setSizePolicy(sizePolicy2)
+        sizePolicy1 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.tabWidget.sizePolicy().hasHeightForWidth())
+        self.tabWidget.setSizePolicy(sizePolicy1)
         self.Player1Tab = QWidget()
         self.Player1Tab.setObjectName(u"Player1Tab")
         self.gridLayout_2 = QGridLayout(self.Player1Tab)
         self.gridLayout_2.setObjectName(u"gridLayout_2")
         self.Player1DartsTable = QTableWidget(self.Player1Tab)
-        if (self.Player1DartsTable.columnCount() < 3):
-            self.Player1DartsTable.setColumnCount(3)
+        if (self.Player1DartsTable.columnCount() < 5):
+            self.Player1DartsTable.setColumnCount(5)
         __qtablewidgetitem = QTableWidgetItem()
         self.Player1DartsTable.setHorizontalHeaderItem(0, __qtablewidgetitem)
         __qtablewidgetitem1 = QTableWidgetItem()
         self.Player1DartsTable.setHorizontalHeaderItem(1, __qtablewidgetitem1)
         __qtablewidgetitem2 = QTableWidgetItem()
         self.Player1DartsTable.setHorizontalHeaderItem(2, __qtablewidgetitem2)
+        __qtablewidgetitem3 = QTableWidgetItem()
+        self.Player1DartsTable.setHorizontalHeaderItem(3, __qtablewidgetitem3)
+        __qtablewidgetitem4 = QTableWidgetItem()
+        self.Player1DartsTable.setHorizontalHeaderItem(4, __qtablewidgetitem4)
         self.Player1DartsTable.setObjectName(u"Player1DartsTable")
-        self.Player1DartsTable.setColumnCount(3)
+        self.Player1DartsTable.setColumnCount(5)
+        self.Player1DartsTable.horizontalHeader().setDefaultSectionSize(75)
         self.Player1DartsTable.horizontalHeader().setStretchLastSection(False)
         self.Player1DartsTable.verticalHeader().setStretchLastSection(False)
 
@@ -100,21 +95,44 @@ class Ui_Scorer(object):
         self.Player2Tab.setObjectName(u"Player2Tab")
         self.gridLayout_3 = QGridLayout(self.Player2Tab)
         self.gridLayout_3.setObjectName(u"gridLayout_3")
-        self.Player1DartsTable_2 = QTableWidget(self.Player2Tab)
-        if (self.Player1DartsTable_2.columnCount() < 3):
-            self.Player1DartsTable_2.setColumnCount(3)
-        __qtablewidgetitem3 = QTableWidgetItem()
-        self.Player1DartsTable_2.setHorizontalHeaderItem(0, __qtablewidgetitem3)
-        __qtablewidgetitem4 = QTableWidgetItem()
-        self.Player1DartsTable_2.setHorizontalHeaderItem(1, __qtablewidgetitem4)
-        __qtablewidgetitem5 = QTableWidgetItem()
-        self.Player1DartsTable_2.setHorizontalHeaderItem(2, __qtablewidgetitem5)
-        self.Player1DartsTable_2.setObjectName(u"Player1DartsTable_2")
-        self.Player1DartsTable_2.setColumnCount(3)
-        self.Player1DartsTable_2.horizontalHeader().setStretchLastSection(False)
-        self.Player1DartsTable_2.verticalHeader().setStretchLastSection(False)
+        self.EndMatchButton = QPushButton(self.Player2Tab)
+        self.EndMatchButton.setObjectName(u"EndMatchButton")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.EndMatchButton.sizePolicy().hasHeightForWidth())
+        self.EndMatchButton.setSizePolicy(sizePolicy2)
 
-        self.gridLayout_3.addWidget(self.Player1DartsTable_2, 0, 0, 1, 1)
+        self.gridLayout_3.addWidget(self.EndMatchButton, 1, 2, 1, 1)
+
+        self.commit_turn_button = QPushButton(self.Player2Tab)
+        self.commit_turn_button.setObjectName(u"commit_turn_button")
+
+        self.gridLayout_3.addWidget(self.commit_turn_button, 1, 0, 1, 1)
+
+        self.Player2DartsTable = QTableWidget(self.Player2Tab)
+        if (self.Player2DartsTable.columnCount() < 5):
+            self.Player2DartsTable.setColumnCount(5)
+        __qtablewidgetitem5 = QTableWidgetItem()
+        self.Player2DartsTable.setHorizontalHeaderItem(0, __qtablewidgetitem5)
+        __qtablewidgetitem6 = QTableWidgetItem()
+        self.Player2DartsTable.setHorizontalHeaderItem(1, __qtablewidgetitem6)
+        __qtablewidgetitem7 = QTableWidgetItem()
+        self.Player2DartsTable.setHorizontalHeaderItem(2, __qtablewidgetitem7)
+        __qtablewidgetitem8 = QTableWidgetItem()
+        self.Player2DartsTable.setHorizontalHeaderItem(3, __qtablewidgetitem8)
+        __qtablewidgetitem9 = QTableWidgetItem()
+        self.Player2DartsTable.setHorizontalHeaderItem(4, __qtablewidgetitem9)
+        self.Player2DartsTable.setObjectName(u"Player2DartsTable")
+        self.Player2DartsTable.setColumnCount(5)
+        self.Player2DartsTable.horizontalHeader().setStretchLastSection(False)
+        self.Player2DartsTable.verticalHeader().setStretchLastSection(False)
+
+        self.gridLayout_3.addWidget(self.Player2DartsTable, 0, 0, 1, 3)
+
+        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.gridLayout_3.addItem(self.horizontalSpacer_2, 1, 1, 1, 1)
 
         self.tabWidget.addTab(self.Player2Tab, "")
 
@@ -144,7 +162,7 @@ class Ui_Scorer(object):
         Scorer.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(Scorer)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 719, 21))
+        self.menubar.setGeometry(QRect(0, 0, 723, 21))
         Scorer.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(Scorer)
         self.statusbar.setObjectName(u"statusbar")
@@ -152,7 +170,7 @@ class Ui_Scorer(object):
 
         self.retranslateUi(Scorer)
 
-        self.tabWidget.setCurrentIndex(1)
+        self.tabWidget.setCurrentIndex(0)
 
 
         QMetaObject.connectSlotsByName(Scorer)
@@ -163,22 +181,31 @@ class Ui_Scorer(object):
         self.NextLegButton.setText(QCoreApplication.translate("Scorer", u">", None))
         self.PreviousLegButton.setText(QCoreApplication.translate("Scorer", u"<", None))
         self.NextSetButton.setText(QCoreApplication.translate("Scorer", u">", None))
-        self.EndMatchButton.setText(QCoreApplication.translate("Scorer", u"End Match", None))
         self.SetNumberLabel.setText(QCoreApplication.translate("Scorer", u"2/4", None))
         self.LegNumberLabel.setText(QCoreApplication.translate("Scorer", u"4/14", None))
         ___qtablewidgetitem = self.Player1DartsTable.horizontalHeaderItem(0)
         ___qtablewidgetitem.setText(QCoreApplication.translate("Scorer", u"Score", None));
         ___qtablewidgetitem1 = self.Player1DartsTable.horizontalHeaderItem(1)
-        ___qtablewidgetitem1.setText(QCoreApplication.translate("Scorer", u"Dart Number", None));
+        ___qtablewidgetitem1.setText(QCoreApplication.translate("Scorer", u"Bounce Out", None));
         ___qtablewidgetitem2 = self.Player1DartsTable.horizontalHeaderItem(2)
-        ___qtablewidgetitem2.setText(QCoreApplication.translate("Scorer", u"BKF", None));
+        ___qtablewidgetitem2.setText(QCoreApplication.translate("Scorer", u"Knock Out", None));
+        ___qtablewidgetitem3 = self.Player1DartsTable.horizontalHeaderItem(3)
+        ___qtablewidgetitem3.setText(QCoreApplication.translate("Scorer", u"Foul", None));
+        ___qtablewidgetitem4 = self.Player1DartsTable.horizontalHeaderItem(4)
+        ___qtablewidgetitem4.setText(QCoreApplication.translate("Scorer", u"Remove", None));
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.Player1Tab), QCoreApplication.translate("Scorer", u"Player 1", None))
-        ___qtablewidgetitem3 = self.Player1DartsTable_2.horizontalHeaderItem(0)
-        ___qtablewidgetitem3.setText(QCoreApplication.translate("Scorer", u"Score", None));
-        ___qtablewidgetitem4 = self.Player1DartsTable_2.horizontalHeaderItem(1)
-        ___qtablewidgetitem4.setText(QCoreApplication.translate("Scorer", u"Dart Number", None));
-        ___qtablewidgetitem5 = self.Player1DartsTable_2.horizontalHeaderItem(2)
-        ___qtablewidgetitem5.setText(QCoreApplication.translate("Scorer", u"BKF", None));
+        self.EndMatchButton.setText(QCoreApplication.translate("Scorer", u"End Match", None))
+        self.commit_turn_button.setText(QCoreApplication.translate("Scorer", u"Commit Turn", None))
+        ___qtablewidgetitem5 = self.Player2DartsTable.horizontalHeaderItem(0)
+        ___qtablewidgetitem5.setText(QCoreApplication.translate("Scorer", u"Score", None));
+        ___qtablewidgetitem6 = self.Player2DartsTable.horizontalHeaderItem(1)
+        ___qtablewidgetitem6.setText(QCoreApplication.translate("Scorer", u"Bounce Out", None));
+        ___qtablewidgetitem7 = self.Player2DartsTable.horizontalHeaderItem(2)
+        ___qtablewidgetitem7.setText(QCoreApplication.translate("Scorer", u"Knock Out", None));
+        ___qtablewidgetitem8 = self.Player2DartsTable.horizontalHeaderItem(3)
+        ___qtablewidgetitem8.setText(QCoreApplication.translate("Scorer", u"Foul", None));
+        ___qtablewidgetitem9 = self.Player2DartsTable.horizontalHeaderItem(4)
+        ___qtablewidgetitem9.setText(QCoreApplication.translate("Scorer", u"Remove", None));
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.Player2Tab), QCoreApplication.translate("Scorer", u"Player 2", None))
         self.PreviousSetButton.setText(QCoreApplication.translate("Scorer", u"<", None))
         self.label_2.setText(QCoreApplication.translate("Scorer", u"Sets", None))
--- dartboard/views/ScorerWindow.py
+++ dartboard/views/ScorerWindow.py
@@ -1,5 +1,6 @@
 from views.Scorer import Ui_Scorer
-from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide2.QtWidgets import QApplication, QMainWindow, QCheckBox, QTableWidgetItem, QWidget, QHBoxLayout, QHeaderView, QPushButton
+from PySide2.QtCore import Qt
 from backend.dartboard_api import get_match_by_id, get_players_by_match_id, get_number_of_legs_by_match_id
 import sys
 
@@ -12,6 +13,78 @@ class ScorerWindow(QMainWindow):
         self.ui = Ui_Scorer()
         self.ui.setupUi(self)
         self.ui.graphicsView.setup_signal(self)
+        self.ui.commit_turn_button.clicked.connect(self.commit_turn)
+
+        header = self.ui.Player1DartsTable.horizontalHeader()
+        header.setSectionResizeMode(QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(0, QHeaderView.Stretch)
+
+        self.addpopulaterow(self.ui.Player1DartsTable, "50", True, False, False)
+        self.addpopulaterow(self.ui.Player1DartsTable, "100", True, False, False)
+
+        
+        
+    def addpopulaterow(self, table, value, bounce, knock, foul):
+        rowPosition = table.rowCount()
+        table.insertRow(rowPosition)
+
+        score = QTableWidgetItem()
+        score.setTextAlignment(Qt.AlignCenter)
+        score.setText(value)
+        table.setItem(rowPosition, 0, score)
+
+        cell_widget = QWidget()
+        chk_bx = QCheckBox()
+        if (bounce):
+            chk_bx.setCheckState(Qt.Checked)
+        else:
+            chk_bx.setCheckState(Qt.Unchecked)
+        layout = QHBoxLayout(cell_widget)
+        layout.addWidget(chk_bx)
+        layout.setAlignment(Qt.AlignCenter)
+        layout.setContentsMargins(0,0,0,0)
+        cell_widget.setLayout(layout)
+        table.setCellWidget(rowPosition, 1, cell_widget)
+
+        cell_widget = QWidget()
+        chk_bx = QCheckBox()
+        if (knock):
+            chk_bx.setCheckState(Qt.Checked)
+        else:
+            chk_bx.setCheckState(Qt.Unchecked)
+        layout = QHBoxLayout(cell_widget)
+        layout.addWidget(chk_bx)
+        layout.setAlignment(Qt.AlignCenter)
+        layout.setContentsMargins(0,0,0,0)
+        cell_widget.setLayout(layout)
+        table.setCellWidget(rowPosition, 2, cell_widget)
+
+        cell_widget = QWidget()
+        chk_bx = QCheckBox()
+        if (foul):
+            chk_bx.setCheckState(Qt.Checked)
+        else:
+            chk_bx.setCheckState(Qt.Unchecked)
+        layout = QHBoxLayout(cell_widget)
+        layout.addWidget(chk_bx)
+        layout.setAlignment(Qt.AlignCenter)
+        layout.setContentsMargins(0,0,0,0)
+        cell_widget.setLayout(layout)
+        table.setCellWidget(rowPosition, 3, cell_widget)
+
+        cell_widget = QWidget()
+        button = QPushButton("X")
+        layout = QHBoxLayout(cell_widget)
+        layout.addWidget(button)
+        layout.setAlignment(Qt.AlignCenter)
+        layout.setContentsMargins(0,0,0,0)
+        cell_widget.setLayout(layout)
+        table.setCellWidget(rowPosition, 4, cell_widget)
+        button.clicked.connect(lambda: self.remove_dart_throw(rowPosition))
+
+    def remove_dart_throw(self, row):
+        print(row)
+        
 
     def enter_match_id(self, match_id):
         self.match = get_match_by_id(match_id)
@@ -38,6 +111,9 @@ class ScorerWindow(QMainWindow):
 
     def dart_thrown(self, msg):
         print("Coming from Scorer: {}".format(msg))
+
+    def commit_turn(self):
+        print("pressed commit turn")
         
 
         
