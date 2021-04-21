4e5939b90c7658796d63d0dc8cc7de51b294300f
Nathan Solomon <nbsolomon4559@gmail.com>
2021-04-20 23:33:04 -0500

    add function to generate players

---
+++ dartboard/backend/generate_players.py
@@ -0,0 +1,13 @@
+from backend.dartboard_api import create_player
+
+
+def generate_players():
+    create_player("Nathan", "Solomon")
+    create_player("Ethan", "Shattuck")
+    create_player("Alex", "Abel-Boozer")
+    create_player("Scean", "Pagani")
+    create_player("Luke", "Skywalker")
+    create_player("Kevin", "Preston")
+    create_player("Ben", "Solomon")
+    create_player("Leia", "Organa")
+    create_player("Han", "Solo")
